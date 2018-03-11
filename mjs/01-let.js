4314f9692659a7de18e64040a3875594f79e92f9
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-03-11 15:57:04 +0700

    let 01

---
+++ mjs/01-let.js
@@ -0,0 +1,9 @@
+/**
+ * Created by TruongDX on 3/9/2018.
+ */
+var message = "this is variable key word var";
+if(true) {
+    let message_let = "this is variable key word let";
+    console.log(message_let);
+}
+console.log(message);
