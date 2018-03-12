1ccc2d397c971ec5c8335c503014fe2724b9b0bb
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-03-13 05:20:20 +0700

    const in es6

--- mjs/01-let.js
+++ mjs/01-let.js
@@ -1,9 +1,5 @@
 /**
  * Created by TruongDX on 3/9/2018.
  */
-var message = "this is variable key word var";
-if(true) {
-    let message_let = "this is variable key word let";
-    console.log(message_let);
-}
-console.log(message);
+const PI = 3.14;
+console.log(PI);
