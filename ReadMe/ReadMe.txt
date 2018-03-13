40c1d8653ea4d54d4a1b722991a6df64a75a256e
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-03-13 23:16:22 +0700

    For in es6

---
+++ ReadMe/ReadMe.txt
@@ -0,0 +1,3 @@
+Dung vong lap for co 3 cach , xem them code
+co 1 chu y, khi dung vong lap for de truy cap doi tuong thi dung cu phap nhu khi truy cap mang
+ngoai ra khong the dung for-of de truy cap thuoc tinh cua doi tuong
--- mjs/01-let.js
+++ mjs/01-let.js
@@ -1,3 +1,40 @@
 /**
  * Created by TruongDX on 3/9/2018.
  */
+let todo = [
+    "Football",
+    "Coding",
+    "Study Script"
+]
+/* ============== Cach 1 viet vong lap for
+let length = todo.length;
+for(let i = 0; i < length; i++){
+    console.log(todo[i]);
+}
+*/
+
+/* ============== Cach 2 viet vong lap for: dung for in
+ for(let index in todo){
+    console.log(todo[index]);
+ }
+ */
+/* ============== Cach 2 viet vong lap for: dung for off
+for(let item of todo){
+    console.log(item);
+}
+ */
+
+let todoObj = {
+    id : 134,
+    name : "Football",
+    status : true
+}
+/*
+for(let index in todoObj){
+    console.log(todoObj[index]);
+}
+*/
+
+for(let item of todoObj){
+    console.log(todoObj[item]);
+}
