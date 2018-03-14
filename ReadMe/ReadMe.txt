adb3c1cbbf2d85525d1c06a29d9bfb60c34aa4ae
DESKTOP-N37IO01\TruongDX <truongdx54@gmail.com>
2018-03-14 21:32:27 +0700

    Spread

---
+++ ReadMe/ReadMe.txt
@@ -0,0 +1,4 @@
+Spread la 1 toan tu trong es6
+cu phap la ... dung de ghep 2 hay nhieu mang, xem them vi du
+
+... la Spread
--- mjs/01-let.js
+++ mjs/01-let.js
@@ -1,3 +1,9 @@
 /**
  * Created by TruongDX on 3/9/2018.
  */
+let courseWeb       = ["HTML", "CSS", "Typescript"];
+let courseMobile    = ["iOS", "Android"];
+let coursePhp       = ["Zend", "Wordpress", "PHP"];
+
+let course = ["HTML", "CSS" , ...courseMobile, "Typescript", ...courseMobile, ...coursePhp];
+console.log(course);
